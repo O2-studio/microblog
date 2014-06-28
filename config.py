2852ae9c1166f4bb94846efc0e6f0d6d37cba084
Ralph Wen <ralph.wen@gmail.com>
2014-06-29 00:44:59 +0800

    login

---
+++ config.py
@@ -0,0 +1,6 @@
+CSRF_ENABLED = True
+SECRET_KEY = 'MY KEEEY'
+
+OPENID_PROVIDERS = [
+        { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
+        { 'name': 'Yahoo', 'url': 'https://me.yahoo.com'}]
