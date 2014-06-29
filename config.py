c3be381703a43cc24fa31bc68af043555098f71f
Yadong Wen <ralph.wen@gmail.com>
2014-06-30 01:57:50 +0800

    reformat according to pep8 with intellij on windows

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
@@ -8,7 +9,7 @@ CSRF_ENABLED = True
 SECRET_KEY = 'MY KEEEY'
 
 OPENID_PROVIDERS = [
-        { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
-        { 'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
-        { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
-        { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
+    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
+    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
+    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
